357162728035f70adcd08bac352893736fb2acce
William Ten Cate <billytencate@gmail.com>
2023-06-17 19:29:12 -0400

    Add erase, change pixel size, and rainbow features

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const resetB = document.querySelector('.reset');
+let rainbow = false;
 
-resetB.addEventListener('click', resetGrid);
+const eraseB = document.querySelector('.erase');
+const sizeB = document.querySelector('.size');
+const rainbowB = document.querySelector('.rainbow');
+
+eraseB.addEventListener('click', eraseGrid);
+sizeB.addEventListener('click', changeSize);
+
+rainbowB.addEventListener('click', () =>{
+    rainbow = !rainbow;
+});
 
 function generateGrid(gridSize = 16){
     const container = document.querySelector('#grid-container');
@@ -13,21 +22,46 @@ function generateGrid(gridSize = 16){
             const row = document.querySelector(`row${i}`);
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.style.cssText = `padding: ${255/gridSize}px`;
             cell.addEventListener('mouseover', () => {
-                cell.classList.add('filled');
+                if(rainbow){
+                    let x = Math.floor(Math.random() * 256);
+                    let y = Math.floor(Math.random() * 256);
+                    let z = Math.floor(Math.random() * 256);
+                    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+                    cell.style.background = bgColor;
+                }
+                else{
+                    cell.style.background = "black";
+                }
             });
             row.appendChild(cell);
         } 
     }
 }
 
-function resetGrid(){
+function eraseGrid(){
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
-        cell.classList.add('.filled');
-        cell.classList.remove('filled');
+        cell.style.background = "none";
     });
 }
 
+function resetGrid(){
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.remove();
+    });
+}
 
+function changeSize(){
+    let gridSize = prompt('How many rows would you like the new grid to have? (Max 100):');
+    resetGrid();
+    if(gridSize > 0 && gridSize <= 100){
+        generateGrid(gridSize);
+    }
+    else{
+        generateGrid();
+    } 
+}
 generateGrid(16);
